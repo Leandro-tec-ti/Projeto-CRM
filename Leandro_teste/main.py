18c62b762c65fd0b135a73fb87bacff0e5666ced
Leandro Fernandes <leandro.tec.ti@hotmail.com>
2025-01-10 16:51:00 -0300

    alteração no cadastro cliente
    usando o Flask para fazer a compilação das informaçoes.

--- Leandro_teste/main.py
+++ Leandro_teste/main.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = "LEANDRO"
 @app.route('/')   #<<--- rota para direcionar pagina de entrada do site "('/')"
 def home():
 
-    return render_template ('login_cliente.html')
+    return render_template ('cadastro.html')
 
 @app.route('/login_cliente', methods=['POST']) #<<--- aqui é uma rota para pagina para fazer o login "('/login')"
 def login():
@@ -25,6 +25,39 @@ def login():
             
         flash('USUÁRIO INVÁLIDO')
         return redirect('/')
+
+@app.route('/cadastro', methods=['POST']) #<<--- aqui é uma rota para pagina para fazer o login "('/cadastro')"
+def cadastro():
+    user = []
+    nome = request.form.get('nome')
+    sobrenome = request.form.get('sobrenome')
+    nascimento = request.form.get('nascimento')
+    cpf = request.form.get('cpf')
+    cnpj = request.form.get('cnpj')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    user = [
+            {
+                "nome": nome,
+                "sobrenome": sobrenome,
+                "nascimento": nascimento,
+                "cpf": cpf,
+                "cnpj": cnpj,
+                "email": email,
+                "senha": senha
+            }
+        ]
+    with open('usuario.json') as usuariotemp :# "whith open" para mostrar o caminho para achar a lista do banco de dados
+        usuarios = json.load(usuariotemp)
+
+    usuario_novo = user + usuarios
+
+    with open('usuario.json', 'w') as gravartemp :# "whith open" para mostrar o caminho para achar a lista do banco de dados
+        json.dump(usuario_novo, gravartemp, indent=9)
+
+    return render_template('/rotavendas.html')
+
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
