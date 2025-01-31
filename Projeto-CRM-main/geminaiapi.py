c564ad517f655f4b3b6c20391565d66174dbfb28
Leandro Fernandes <leandro.tec.ti@hotmail.com>
2025-01-30 23:30:46 -0300

    Atualização

--- Projeto-CRM-main/geminaiapi.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDg3H8ObRz-Ba8co8u_pC1NLhckkrC3oas")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("quantos anos o mundo tem?")
-print(response.text)
